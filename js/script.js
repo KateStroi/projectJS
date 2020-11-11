3c4a2f10ba074d8ef2c29da8a880d06afc235ae7
Kate <catia.stroi@gmail.com>
2020-11-11 09:31:37 +0200

    practics for conditions and cycles

--- js/script.js
+++ js/script.js
@@ -8,15 +8,60 @@ const personalMovieDB = {
     privat: false,
 };
 
-let a = prompt('Which last of the film did you watch?', ""),
-    b = prompt('Please, write rating of this film', "1-5"),
-    c = prompt('Which last of the film did you watch?', ""),
-    d = prompt('Please, write rating of this film', "1-5");
+
+
+if (personalMovieDB.count >= 20 && personalMovieDB.count < 30) {
+    alert("Filmwatcher");
+} else if (personalMovieDB.count < 20 ) {
+    alert ("Not filmwatcher");
+} else {
+    alert("Error");
+}
+
+let countQuestion = 0;
+for( let i=0; i < 2; i++) {
+
+    let a = prompt('Which last of the film did you watch?', ""),
+        b = prompt('Please, write rating of this film', "");
+    if ( a === "") {
+        alert("Non empty, try again");
+    } else if ( a == null) { 
+        i--;
+    } else { 
+        personalMovieDB.movies[a] = b;
+    }
+}
+// while (countQuestion <1) {
+//     let a = prompt('Which last of the film did you watch?', ""),
+//         b = prompt('Please, write rating of this film', "");
+
+//     // switch (a) {
+//     //     case "": 
+//     //         alert("Non empty, try again");
+//     //         break;
+//     //     case a.length > 12 || a == null :  
+//     //         while (countQuestion <2) {
+//     //             alert(a); 
+//     //             alert(b); 
+//     //             personalMovieDB.movies[a] = b; 
+//     //             countQuestion ++;
+//     //         }
+//     //         break;
+//     // }
+
+//     if ( a === "") {
+//         alert("Non empty, try again");
+//     } else if ( a == null) { 
+//         a = prompt('Which last of the film did you watch?', "");
+//         b = prompt('Please, write rating of this film', ""); 
+//     } else { 
+//         personalMovieDB.movies[a] = b; 
+//         countQuestion ++;
+//     }
+// }
 
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 console.log(personalMovieDB.movies);
 
  
