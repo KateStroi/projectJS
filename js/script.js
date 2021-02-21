6ee75df6ce2822505899518d2c3872a9cc9ce699
Kate <catia.stroi@gmail.com>
2021-02-21 22:19:50 +0200

    practice with actions on page site

--- js/script.js
+++ js/script.js
@@ -1,84 +1,53 @@
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+/* Задания на урок:
 
-    start: function() {
-        personalMovieDB.count = +prompt("How many films have you watched?", ""); 
-     
-         while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-             personalMovieDB.count = +prompt("How many films have you watched?", "");
-         }
-     },
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-    rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            let lastFilm = prompt("One of the latest films", ""),
-                rating = +prompt("How many point you can give for this film?", "");
-        
-            if (lastFilm != '' && rating!= '' && lastFilm != null && rating != null && lastFilm.length <= 50) {
-                personalMovieDB.movies[lastFilm] = rating;
-        
-                lastFilm = prompt("One of the latest films", "");
-                rating = prompt("How many point you can give for this film?", "");
-            } else if (lastFilm.length >= 50) {
-                console.log("Error");
-                i--; 
-            } 
-        }  
-    },
-     
-    detectPersonalLevel: function() {
-        let countMovie = personalMovieDB.count;
-        
-        if (countMovie < 10) {
-            console.log("So small count of the watched films" + countMovie);
-        } else if (countMovie >= 10 && countMovie < 30) {
-            console.log("You a classic watcher");
-        } else if (countMovie >= 30) {
-            console.log("You are films lover");
-        }
-    },
-    
-    showMyBD: function(isPivat) {
-        if ( !isPivat) {
-            console.log(personalMovieDB);
-        }
-    },
+2) Изменить жанр фильма, поменять "комедия" на "драма"
 
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
 
-    writeYourGenres: function() {   
-        for(let i = 0; i <= 2; i++) {
-            let genresName = prompt(`Your favourite genre is number ${i + 1}`, "");
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
 
-            if (genresName != "" && genresName != null) {
-                personalMovieDB.genres[i] = genresName;
-            } else { 
-                genresName = prompt(`Your favourite genre is number ${i + 1}`, ""); 
-                personalMovieDB.genres[i] = genresName;
-            }
-        } 
-    },
+5) Добавить нумерацию выведенных фильмов */
 
-    toggleVisibleMyBD: function(statePrivate) {
-        if(!personalMovieDB.privat) {
-            personalMovieDB.privat = true;        
-        } else { 
-            personalMovieDB.privat = false;
-        }
-    },
+'use strict';
 
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
 };
 
-// personalMovieDB.start();
-// personalMovieDB.rememberMyFilms();
-// personalMovieDB.detectPersonalLevel();
-personalMovieDB.showMyBD(personalMovieDB.privat);
-personalMovieDB.writeYourGenres();
-// personalMovieDB.toggleVisibleMyBD();
+const promoAdv = document.querySelectorAll('.promo__adv img'), 
+      poster = document.querySelector('.promo__bg'),
+      ganre = poster.querySelector('.promo__genre'),
+      moviesList = document.querySelector('.promo__interactive-list'),
+      movies = movieDB.movies;
 
-personalMovieDB.genres.forEach((item, i) => {
-    console.log(`Favourite ganre ${i+1} is call ${personalMovieDB.genres[i]}`)
+let  resultMovie = '';
+
+promoAdv.forEach(element => {
+    element.remove();
 });
+
+poster.style.cssText = 'background-image: url(img/bg.jpg)';
+
+ganre.textContent = "драма"; 
+
+moviesList.innerHTML = "";
+
+movies.sort();
+
+movies.forEach((item, i) => {
+    resultMovie += `<li class="promo__interactive-item">${i+1}. ${item} <div class=\"delete\"></div></li>`;
+});
+
+moviesList.insertAdjacentHTML ("afterbegin", resultMovie);
+
+console.log(resultMovie);
