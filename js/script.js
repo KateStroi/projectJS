52dfde86f22bd53bf394eb3ccbca68a50d91016b
Kate <catia.stroi@gmail.com>
2021-03-04 23:05:39 +0200

    update list, cut movie, delete, sort movie from movie bd

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту 
+
+*/
 
 'use strict';
+
+// Возьмите свой код из предыдущей практики 
 
 const movieDB = {
     movies: [
@@ -28,26 +35,74 @@ const promoAdv = document.querySelectorAll('.promo__adv img'),
       poster = document.querySelector('.promo__bg'),
       ganre = poster.querySelector('.promo__genre'),
       moviesList = document.querySelector('.promo__interactive-list'),
-      movies = movieDB.movies;
-
-let  resultMovie = '';
-
+      movies = movieDB.movies,
+      formAdd = document.querySelector('.add'),
+      formButton = formAdd.querySelector('button'),
+      formInput = formAdd.querySelector('.adding__input'),
+      checkbox = formAdd.querySelector('[type="checkbox"]');
+ 
 promoAdv.forEach(element => {
     element.remove();
 });
 
-poster.style.cssText = 'background-image: url(img/bg.jpg)';
+poster.style.backgroundImage = 'url("img/bg.jpg")';
 
 ganre.textContent = "драма"; 
 
-moviesList.innerHTML = "";
+let deleteMovie = function()  {
+    console.log('delete');
+};
+ 
+let setDeleteBtnsListener = () => {
+    const deleteBtns = document.querySelectorAll('.delete');
 
-movies.sort();
+    deleteBtns.forEach(deleteBtn => {
+        deleteBtn.addEventListener('click', deleteMovie);
+        console.log(deleteBtn);
+    }); 
+};
 
-movies.forEach((item, i) => {
-    resultMovie += `<li class="promo__interactive-item">${i+1}. ${item} <div class=\"delete\"></div></li>`;
-});
+let addMovie = function() { 
+    let resultMovie = '';
 
-moviesList.insertAdjacentHTML ("afterbegin", resultMovie);
+    moviesList.innerHTML = ""; 
+    movies.sort();
 
-console.log(resultMovie);
+    movies.forEach((item, i) => { 
+        if (item.length > 5 ) {
+            resultMovie += `<li class="promo__interactive-item">${i+1}. ${item.slice(0, 5)}... <div class=\"delete\"></div></li>`; 
+        } else { 
+            resultMovie += `<li class="promo__interactive-item">${i+1}. ${item} <div class=\"delete\"></div></li>`; 
+        }
+    }); 
+
+    moviesList.insertAdjacentHTML("afterbegin", resultMovie);
+    setDeleteBtnsListener();
+};
+ 
+addMovie();
+
+const formValueResult = (e) => {
+    let resultValue = String(formInput.value);
+    e.preventDefault();
+
+    if (resultValue != "") {
+        movies.push(resultValue.toUpperCase()); 
+        addMovie();
+    } 
+};  
+
+formButton.addEventListener('click', formValueResult);
+
+let checkboxProperty = (e) => {
+    console.log(e.target);
+    console.log(e.type);
+    console.log(checkbox.value);
+    if (checkbox.checked == true){
+        console.log("To do the favourite");
+      } else {
+        console.log("Delete from the favouretes");
+      }
+};
+
+checkbox.addEventListener('click', checkboxProperty);
