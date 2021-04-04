913d49f368b5dc1231595c42cb4ae74fa3f8d5ad
Kate <catia.stroi@gmail.com>
2021-04-04 16:57:41 +0300

    added nav to the slider

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -2454,6 +2454,65 @@ window.addEventListener('DOMContentLoaded', () => {
   offerSlides.forEach(slide => {
     slide.style.width = width;
   });
+  offerSlider.style.position = 'relative';
+  const indicators = document.createElement('ol'),
+        dots = [];
+
+  function setActiveDot() {
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[offerSlideIndex - 1].style.opacity = 1;
+  }
+
+  function addZero() {
+    if (offerSlides.length < 10) {
+      current.textContent = `0${offerSlideIndex}`;
+    } else {
+      current.textContent = offerSlideIndex;
+    }
+  }
+
+  indicators.classList.add('carousel-indicators');
+  indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+  offerSlider.append(indicators);
+
+  for (let i = 0; i < offerSlides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+
+    if (i == 0) {
+      dot.style.opacity = "1";
+    }
+
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
   btnNext.addEventListener('click', () => {
     if (offset == +width.slice(0, width.length - 2) * (offerSlides.length - 1)) {
       offset = 0;
@@ -2469,11 +2528,8 @@ window.addEventListener('DOMContentLoaded', () => {
       offerSlideIndex++;
     }
 
-    if (offerSlides.length < 10) {
-      current.textContent = `0${offerSlideIndex}`;
-    } else {
-      current.textContent = offerSlideIndex;
-    }
+    addZero();
+    setActiveDot();
   });
   btnPrev.addEventListener('click', () => {
     if (offset == 0) {
@@ -2490,11 +2546,18 @@ window.addEventListener('DOMContentLoaded', () => {
       offerSlideIndex--;
     }
 
-    if (offerSlides.length < 10) {
-      current.textContent = `0${offerSlideIndex}`;
-    } else {
-      current.textContent = offerSlideIndex;
-    }
+    addZero();
+    setActiveDot();
+  });
+  dots.forEach(dot => {
+    dot.addEventListener('click', e => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+      offerSlideIndex = slideTo;
+      offset = offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offerSlidesField.style.transform = `translateX(-${offset}px)`;
+      addZero();
+      setActiveDot();
+    });
   });
 });
 
